19f19850cc3e46a468670cb66a6964348760104f
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2021-03-04 18:03:58 -0500

    defin scopes for getting all recipes and recipes by i

--- lib/vmodels/recipe.rb
+++ lib/vmodels/recipe.rb
@@ -2,18 +2,41 @@ require 'contentful'
 require 'byebug'
 
 class Recipe
-  def all
+
+  #
+  # Instance Methods
+  #
+
+  def initialize(client_entry)
+    @title = client_entry.title
+    @image_url = client_entry.photo
   end
 
-  def by_id(id)
-  end
+  #
+  # Class Methods
+  #
 
-  def client
-    @client ||= Contentful::Client.new(
-      access_token: ENV['CONTENTFUL_ACCESS_TOKEN'],
-      space: ENV['CONTENTFUL_SPACE_ID'],
-      dynamic_entries: :auto,
-      raise_errors: true
-    )
+  class << self
+    attr_accessor :client
+
+    def all
+      client.entries(content_type: 'recipe').map do |entry|
+        self.new(entry)
+      end
+    end
+
+    def by_id(id)
+      self.new(client.entry(id))
+    end
+
+    def client
+      @client ||= Contentful::Client.new(
+        access_token: ENV['CONTENTFUL_ACCESS_TOKEN'],
+        space: ENV['CONTENTFUL_SPACE_ID'],
+        dynamic_entries: :auto,
+        raise_errors: true
+      )
+    end
   end
 end
+
--- spec/vmodels/recipe_spec.rb
+++ spec/vmodels/recipe_spec.rb
@@ -1,33 +1,46 @@
 require 'spec_helper'
 
 RSpec.describe Recipe do
-  context '#all' do
-    it 'should retrieve all recipes from Contenful web service' do
+  context 'scopes' do
+    context '#all' do
+      it 'should retrieve all recipes from Contenful web service' do
+        VCR.use_cassette('success_get_all_entries') do
+          recipes = Recipe.all
+          expect(recipes).to be_a Array
+          recipes.each do |recipe|
+            expect(recipe).to be_a Recipe
+          end
+        end
+      end
     end
-  end
 
-  context '#by_id' do
-    it 'should bring an specific record from Contentful web service' do
-    end
+    context '#by_id' do
+      it 'should bring an specific record from Contentful web service' do
+        VCR.use_cassette('success_get_entry') do
+          expect(Recipe.by_id('4dT8tcb6ukGSIg2YyuGEOm')).to be_a Recipe
+        end
+      end
 
-    it 'raises an error when the record is not found' do
+      it 'raises an error when the record is not found' do
+      end
     end
   end
 
   context '#client' do
     it 'should create instance for the Contentful client' do
       VCR.use_cassette('success_client_config') do
-        expect(subject.client).to be_a Contentful::Client
+        expect(Recipe.client).to be_a Contentful::Client
       end
     end
 
     context 'when there is a wrong configuration' do
       before :each do
+        Recipe.client = nil
         ENV['CONTENTFUL_SPACE_ID'] = nil
       end
 
       it 'raises an error saying about the argument error' do
-        expect { subject.client }.to raise_error(ArgumentError)
+        expect { Recipe.client }.to raise_error(ArgumentError)
       end
     end
   end
